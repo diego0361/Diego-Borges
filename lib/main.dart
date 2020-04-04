0f334bc1ea36d8a86e2dcf0c1035de83a42618e8
Diego Borges <borgesdiego00@gmail.com>
2020-04-03 23:53:17 -0300

    Inicio

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Programa Recicla"), centerTitle: true,),
+
+      body: Container(child: 
+      Stack(children: <Widget>[
+        CustomScrollView(
+          primary: false,
+          slivers: <Widget>[
+            SliverPadding(
+              padding: const EdgeInsets.all(20),
+              sliver: SliverGrid.count(
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: Image.asset('imagens/flut1.png'),
+                  
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('He\'d have you all unravel at the'),
+                    color: Colors.green[100],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Heed not the rabble'),
+                    color: Colors.green[200],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Sound of screams but the'),
+                    color: Colors.green[300],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Who scream'),
+                    color: Colors.green[400],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Revolution is coming...'),
+                    color: Colors.green[500],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Revolution, they...'),
+                    color: Colors.green[600],
+                  ),
+
+                ],
+              ),
+            ),
+          ],
+        )
+      ]
+    )
+  )
+  
+) ;
+    
+}
+}
